c480e6a17fa60c53e5f0d9f727b7a499185ffbd8
naiaragabriela <naigabi31@gmail.com>
2023-03-09 13:27:58 -0300

    exercicios em aula

--- src/ProgramaMatriz/Program.cs
+++ src/ProgramaMatriz/Program.cs
@@ -1,140 +1,202 @@
 ﻿Console.WriteLine("--------------------------------------  EXERCICIO 01-----------------------------------------------------------");
 
-double[,] matrizMedia = new double[5, 3];
+double[,] mat = new double[5, 3];
+
+Random sorteio = new Random();
 
 
-for (int linha = 0; linha < 5; linha++)
+//Carregar os valores da matriz
+
+for(int c =0; c < mat.GetLength(1); c++)
 {
-    for (int coluna = 0; coluna < 3; coluna++)
+    for(int l=0; l < mat.GetLength(0); l++)
     {
-        if (coluna == 2)
-        {
-            matrizMedia[linha, 2] = (matrizMedia[linha, 0] + matrizMedia[linha, 1]) / 2;
+        mat[c,l] = sorteio.Next(10);
+    }
+}
 
-        }
-        else
-        {
-            matrizMedia[linha, coluna] = linha * coluna;
-        }
+// calculo da terceira coluna 
+Console.WriteLine("Calculando...");
 
-
-        Console.Write("|" + matrizMedia[linha, coluna]);
+    for (int l = 0; l < mat.GetLength(0); l++)
+    {
+        mat[l,2] = (mat[l,0]+ mat[l,1]) / 2;
     }
 
+//impressao da matriz
+Console.WriteLine("Os valores da matriz são: ");
+for (int l = 0; l < mat.GetLength(0); l++)
+{
+    for (int c = 0; c < mat.GetLength(1); c++)
+    {
+        Console.Write("\tmat[" + l + "][" + c + "]=" + mat[l,c].ToString("F2"));
+    }
     Console.WriteLine();
 }
+
 
 Console.WriteLine("----------------------------------------EXERCÍCIO 02---------------------------------------------------------");
 
-double[,] matrizOriginal = new double[5, 5];
+int[,] mat1 = new int[5, 5];
+Random sorteio1 = new Random();
 
-double somaDasLinhas = 0;
-double somaDasColunas = 0;
-double somaDaDiagonalPrincipal = 0;
-double somaDaDiagonalSecundaria = 0;
-
-
-for (int linha = 0; linha < 5; linha++)
+for (int c=0; c< mat1.GetLength(1); c++)
 {
-    for (int coluna = 0; coluna < 5; coluna++)
+    for(int l=0; l<mat1.GetLength(0); l++)
     {
-        matrizOriginal[linha, coluna] = linha * coluna;
-        Console.Write("|" + matrizOriginal[linha, coluna]);
+        mat1[l, c] = sorteio1.Next(1000);
     }
-
-    Console.WriteLine();
 }
 
-Console.WriteLine("--------------------------------------------------------");
-
-for (int linha = 0; linha < 5; linha++)
+Console.WriteLine("Os valores da matriz são: ");
+for (int l = 0; l < mat1.GetLength(0); l++)
 {
-    for (int coluna = 0; coluna < 5; coluna++)
+    for (int c = 0; c < mat1.GetLength(1); c++)
     {
-        somaDasLinhas += matrizOriginal[linha, coluna];
+        Console.Write("\tmat[" + l + "][" + c + "]=" + mat1[l, c].ToString("F2"));
     }
-    Console.WriteLine("A soma da linha " + linha + " é igual a: " + somaDasLinhas);
-    somaDasLinhas = 0;
-    Console.WriteLine();
-}
-
-Console.WriteLine("--------------------------------------------------------");
-
-
-for (int coluna = 0; coluna < 5; coluna++)
-{
-    for (int linha = 0; linha < 5; linha++)
-    {
-        somaDasColunas += matrizOriginal[linha, coluna];
-    }
-
-    Console.WriteLine("A soma da coluna " + coluna + " é igual a: " + somaDasColunas);
-    somaDasColunas = 0;
     Console.WriteLine();
 }
 
 
-Console.WriteLine("------------------------------------------------------");
+//soma das linhas
 
-for (int linha = 0; linha < 5; linha++)
+Console.WriteLine("Soma das Linhas:");
+for(int l =0; l < mat1.GetLength(0); l++)
 {
-    for (int coluna = 0; coluna < 5; coluna++)
+    Console.Write("Soma da linha [" + l + "]= ");
+    int somaLinha = 0;
+    for (int c = 0; c < mat1.GetLength(1); c++)
     {
-        if (linha == coluna)
-        {
-            somaDaDiagonalPrincipal += matrizOriginal[linha, coluna];
-        }
+        //somalinha = somalinha + mat[l,c];
+        somaLinha += mat1[l, c];
     }
+    Console.WriteLine(somaLinha);
+}
+// soma das colunas 
+Console.WriteLine("Soma das Colunas: ");
+for(int c= 0; c< mat1.GetLength(1); c++)
+{
+    Console.WriteLine("Soma da Coluna [" + c + "]");
+    int somaColuna = 0;
+    for (int l = 0; l < mat1.GetLength(0); l++)
+    {
+        somaColuna += mat1[l,c];
+    }
+    Console.WriteLine(somaColuna);
 }
 
-Console.WriteLine("A soma da diagonal primária é " + somaDaDiagonalPrincipal);
-
-for (int linha = 0; linha < 5; linha++)
+//Soma da diagonal principal
+Console.Write("Soma da Diagonal Principal..");
+int diagonal = 0;
+for (int c= 0;c< mat1.GetLength(1); c++)
 {
-    for (int coluna = 0; coluna < 5; coluna++)
-    {
-        if ((linha + coluna) == 4)
-        {
-            somaDaDiagonalSecundaria += matrizOriginal[linha, coluna];
-        }
-    }
+    diagonal += mat1[c, c];
+}
+Console.WriteLine(diagonal);
+
+
+// Soma diagonal Secundaria
+Console.Write("Soma da Diagonal Segundária");
+int diagonalsecundaria = 0;
+
+for (int c = 0; c < mat1.GetLength(1); c++)
+{
+    diagonalsecundaria += mat1[c, mat1.GetLength(1)-c-1];
 
 }
-
-Console.WriteLine("A soma da diagonal secundária é " + somaDaDiagonalSecundaria);
-
-
+Console.WriteLine(diagonalsecundaria);
 
 
 
 
 Console.WriteLine("----------------------------------------EXERCÍCIO 03---------------------------------------------------------");
 
-double[,] matrizInversa = new double[3, 3];
+int[,] mat2 = new int[3, 3];
+int[,] mat3 = new int[3, 3];
 
 
-for (int linha = 0; linha < 3; linha++)
+Random sorteio2 = new Random();
+
+
+//Carregar os valores da matriz
+
+
+
+for (int l = 0; l < mat2.GetLength(0); l++)
 {
-    for (int coluna = 0; coluna < 3; coluna++)
+    for (int c = 0; c < mat2.GetLength(1); c++)
     {
-        matrizInversa[linha, coluna] = linha * coluna;
-        Console.Write("|" + matrizInversa[linha, coluna]);
+        mat2[l,c] = sorteio2.Next(10);
     }
-
     Console.WriteLine();
+
+}
+
+//matriz inversa
+int ultimaLinha= mat.GetLength(0)-1;
+int ultimaColuna = mat.GetLength(1)-1;
+
+
+for (int l = 0; l < mat2.GetLength(0); l++)
+{
+    for (int c = 0; c < mat2.GetLength(1); c++)
+    {
+        mat3[ultimaLinha - l , ultimaColuna - c] = mat2[l, c]; 
+    }
+    Console.WriteLine();
+
 }
 
 
-Console.WriteLine("------------------------------------------------------");
 
 
 
-for (int linha = 2; linha >= 0; linha--)
+//impressao da matriz
+Console.WriteLine("Os valores da matriz são: ");
+
+for (int l = 0; l < mat3.GetLength(0); l++)
 {
-    for (int coluna = 2; coluna >= 0; coluna--)
-    {
-        Console.Write("|" + matrizInversa[linha, coluna]);
-    }
+    for (int c = 0; c < mat3.GetLength(1); c++)
 
+    {
+
+
+        Console.Write("\tmat3[" + l + "][" + c + "]=" + mat3[l, c].ToString("d4"));
+    }
     Console.WriteLine();
 }
+    Console.WriteLine("**************************************************************************************************");
+
+
+
+
+
+//outra forma
+//matriz inversa
+
+
+for (int l = 0, l2 = mat2.GetLength(0) - 1; l < mat2.GetLength(0); l++, l2--)
+{
+    for (int c = 0, c2 = mat2.GetLength(1); c < mat2.GetLength(1); c++, c2--)
+    {
+        mat3[l2, c2] = mat2[l, c];
+    }
+    Console.WriteLine();
+}
+
+        //impressao da matriz
+        Console.WriteLine("Os valores da matriz são: ");
+
+    for (int l = 0; l < mat3.GetLength(0); l++)
+    {
+        for (int c = 0; c < mat3.GetLength(1); c++)
+
+        {
+
+
+            Console.Write("\tmat3[" + l + "][" + c + "]=" + mat3[l, c].ToString("d4"));
+        }
+        Console.WriteLine();
+    }
+   
